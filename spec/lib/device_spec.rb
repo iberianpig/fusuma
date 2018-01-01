b8b581eae54f84f2569cd7e646ae8781c11fbfbb
iberianpig <yhkyky@gmail.com>
2018-01-02 06:33:18 +0900

    rename filename

--- spec/lib/device_spec.rb
+++ spec/lib/device_spec.rb
@@ -3,29 +3,25 @@ module Fusuma
   describe 'Device' do
     describe '.name' do
       context 'with no tap to click device (like a bluetooth apple trackpad)' do
-        let(:apple_bluetooth_keyboard_log) do
-          File.open(
-            'spec/lib/libinput-list-devices_apple_bluetooth_keyboard.txt'
-          )
+        let(:magic_trackpad_log) do
+          File.open('spec/lib/libinput-list-devices_magic_trackpad.txt')
         end
         it 'should return array' do
           allow(Open3).to receive(:popen3).with('libinput-list-devices')
-            .and_return(apple_bluetooth_keyboard_log)
+            .and_return(magic_trackpad_log)
           expect(Device.names.class).to eq Array
         end
 
         it 'should return correct devices' do
           allow(Open3).to receive(:popen3).with('libinput-list-devices')
-            .and_return(apple_bluetooth_keyboard_log)
+            .and_return(magic_trackpad_log)
           expect(Device.names).to eq %w(event8 event9)
         end
       end
 
       context 'when no devices' do
         let(:unavailable_log) do
-          File.open(
-            'spec/lib/libinput-list-devices_unavailable.txt'
-          )
+          File.open('spec/lib/libinput-list-devices_unavailable.txt')
         end
 
         it 'should failed with exit' do
