2bb27b3da9c12de2bdb2dc550b9ebc8c42a5e5c0
iberianpig <yhkyky@gmail.com>
2018-06-12 00:10:07 +0900

    fix specs when not installing libinput-tools

--- spec/fusuma_spec.rb
+++ spec/fusuma_spec.rb
@@ -4,32 +4,34 @@ module Fusuma
   describe Runner do
     describe '.run' do
       before do
-        Singleton.__init__(Fusuma::MultiLogger)
-        Singleton.__init__(Fusuma::Config)
+        Singleton.__init__(MultiLogger)
+        Singleton.__init__(Config)
       end
 
       context 'when without option' do
         it 'should not enable debug mode' do
-          allow_any_instance_of(Fusuma::Runner).to receive(:run)
-          Fusuma::Runner.run
-          expect(Fusuma::MultiLogger.instance).not_to be_debug_mode
+          allow_any_instance_of(Runner).to receive(:run)
+          Runner.run
+          expect(MultiLogger.instance).not_to be_debug_mode
         end
       end
 
       context 'when run with argument "-v"' do
         it 'should enable debug mode' do
-          allow_any_instance_of(Fusuma::Runner).to receive(:run)
-          Fusuma::MultiLogger.send(:new)
-          Fusuma::Runner.run(verbose: true)
-          expect(Fusuma::MultiLogger.instance).to be_debug_mode
+          allow_any_instance_of(Runner).to receive(:run)
+          allow_any_instance_of(LibinputCommands).to receive(:version)
+            .and_return("test version\n")
+          MultiLogger.send(:new)
+          Runner.run(verbose: true)
+          expect(MultiLogger.instance).to be_debug_mode
         end
       end
 
       context 'when run with argument "-c path/to/config.yml"' do
         it 'should assign custom_path' do
-          allow_any_instance_of(Fusuma::Runner).to receive(:run)
-          config = Fusuma::Config.instance
-          expect { Fusuma::Runner.run(config_path: 'path/to/config.yml') }
+          allow_any_instance_of(Runner).to receive(:run)
+          config = Config.instance
+          expect { Runner.run(config_path: 'path/to/config.yml') }
             .to change { config.custom_path }.from(nil).to('path/to/config.yml')
         end
       end
