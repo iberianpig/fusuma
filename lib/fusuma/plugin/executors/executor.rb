c80d964a4cc7b79b479740386796f7aeb8907be1
iberianpig <yhkyky@gmail.com>
2019-04-29 15:41:34 +0900

    fix spec/lib/plugin/executors/command_executor_spec.rb

--- lib/fusuma/plugin/executors/executor.rb
+++ lib/fusuma/plugin/executors/executor.rb
@@ -25,18 +25,11 @@ module Fusuma
         def execute(_vector)
           raise NotImplementedError, "override #{self.class.name}##{__method__}"
         end
-
-        # Set source for tag from config.yml.
-        # DEFAULT_SOURCE is defined in each Executor plugins.
-        def source
-          @source ||= options.fetch(:source,
-                                    self.class.const_get('DEFAULT_SOURCE'))
-        end
       end
 
       # Generate executor
       class Generator
-        # @param options [Hash] like a { executor: { dummy: 'dummy_options'  }  }
+        # @param options [Hash] like a { executors: { dummy: 'dummy_options'  }  }
         def initialize(options:)
           @options = options.fetch(:executors, {})
         end
