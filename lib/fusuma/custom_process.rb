215f53ab9cdbefefba4ec4233a1990f65d18076e
iberianpig <yhkyky@gmail.com>
2022-03-14 06:27:15 +0900

    feat(custom_process): Allow changing the process name on the sub-process side

--- lib/fusuma/custom_process.rb
+++ lib/fusuma/custom_process.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
-require 'fusuma/string_support'
+require "fusuma/string_support"
 
 module Fusuma
   # Rename process
   module CustomProcess
+    attr_writer :proctitle
+
     def fork
       Process.fork do
-        Process.setproctitle(self.class.name.underscore.to_s)
+        Process.setproctitle(proctitle)
         yield
       end
+    end
+
+    def proctitle
+      @proctitle ||= self.class.name.underscore
     end
   end
 end
