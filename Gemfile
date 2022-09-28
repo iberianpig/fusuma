58692d0dd99575a8bf9ca25294b7455f208cf064
iberianpig <yhkyky@gmail.com>
2022-09-28 14:19:18 +0900

    feat(lint): use standardrb

--- Gemfile
+++ Gemfile
@@ -1,24 +1,23 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 # Specify your gem's dependencies in fusuma.gemspec
 gemspec
 
-gem 'bump'
-gem 'bundler'
-gem 'github_changelog_generator', '~> 1.16'
-gem 'pry-byebug', '~> 3.4'
-gem 'pry-doc'
-gem 'pry-inline'
-gem 'rake', '~> 13.0'
-gem 'rblineprof'
-gem 'rblineprof-report'
-gem 'reek'
-gem 'rspec', '~> 3.0'
-gem 'rspec-parameterized'
-gem 'rubocop'
-gem 'rubocop-rake'
-gem 'rubocop-rspec'
-gem 'simplecov'
-gem 'yard'
+gem "bump"
+gem "bundler"
+gem "github_changelog_generator", "~> 1.16"
+gem "pry-byebug", "~> 3.4"
+gem "pry-doc"
+gem "pry-inline"
+gem "rake", "~> 13.0"
+gem "rblineprof"
+gem "rblineprof-report"
+gem "reek"
+gem "rspec", "~> 3.0"
+gem "rspec-parameterized"
+gem "rubocop"
+gem "simplecov"
+gem "standard", require: false
+gem "yard"
--- spec/lib/custom_process_spec.rb
+++ spec/lib/custom_process_spec.rb
@@ -1,28 +1,38 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-require './lib/fusuma/custom_process'
+require "spec_helper"
+require "./lib/fusuma/custom_process"
 
 module Fusuma
   RSpec.describe CustomProcess do
-    class ForkTest
-      include CustomProcess
+    before do
+      test_klass = Class.new do
+        include CustomProcess
 
-      def call
-        fork { puts 'hoge' }
+        def call
+          fork { puts "hoge" }
+        end
       end
+      stub_const("TestKlass", test_klass)
     end
 
-    describe '.fork' do
+    describe ".fork" do
       before do
-        @test_instance = ForkTest.new
-      end
-      it 'call Process.fork and Process.setproctitle' do
-        expect(Process).to receive(:fork).and_yield do |block_context|
+        @test_instance = TestKlass.new
+        allow(Process).to receive(:fork).and_yield do |block_context|
           allow(block_context).to receive(:proctitle).and_return(@test_instance.proctitle)
-          expect(Process).to receive(:setproctitle).with(@test_instance.proctitle)
+          allow(Process).to receive(:setproctitle).with(@test_instance.proctitle)
         end
+      end
+
+      it "call Process.fork" do
         @test_instance.call
+        expect(Process).to have_received(:fork)
+      end
+
+      it "Process.setproctitle" do
+        @test_instance.call
+        expect(Process).to have_received(:setproctitle).with(@test_instance.proctitle)
       end
     end
   end
