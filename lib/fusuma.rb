d3a50b9493df105ff54d23d90062ce87415dd960
Carlos Esparza <ca2010es@gmail.com>
2023-07-22 12:10:34 -0700

    fix adding timer to inputs twice

--- lib/fusuma.rb
+++ lib/fusuma.rb
@@ -59,7 +59,6 @@ module Fusuma
       @inputs = Plugin::Inputs::Input.plugins.map do |cls|
         cls.ancestors.include?(Singleton) ? cls.instance : cls.new
       end
-      @inputs.push(Plugin::Inputs::TimerInput.instance)
       @filters = Plugin::Filters::Filter.plugins.map(&:new)
       @parsers = Plugin::Parsers::Parser.plugins.map(&:new)
       @buffers = Plugin::Buffers::Buffer.plugins.map(&:new)
