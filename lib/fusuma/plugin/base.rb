2eff6928dd6fe3372a0573eb47fcb8c1099e1bc8
iberianpig <yhkyky@gmail.com>
2023-09-02 14:38:06 +0900

    fix(plugin): forgot to change variable name

--- lib/fusuma/plugin/base.rb
+++ lib/fusuma/plugin/base.rb
@@ -54,7 +54,7 @@ module Fusuma
             next if param_types.any? { |klass| val.is_a?(klass) }
 
             MultiLogger.error("Please fix config.yml.")
-            MultiLogger.error(":#{base.keys.map(&:symbol)
+            MultiLogger.error(":#{config_index.keys.map(&:symbol)
             .join(" => :")} => :#{key} should be #{param_types.join(" OR ")}.")
             exit 1
           end
